58b7392dcba5dd4c1464aa23910028aafc5df62e
ayana <ayanayugay25@gmail.com>
2025-05-15 16:58:00 +0600

    Добавлен футер сайта

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <p>© 2025 Singapore Travel. Все права защищены.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
