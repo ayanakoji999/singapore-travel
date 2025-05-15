df21e9ca712f83f04d8e10fc72ba6ee42d2c48b8
ayana <ayanayugay25@gmail.com>
2025-05-15 16:50:51 +0600

    Добавлен блок Контакты в нижней части сайта

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,14 @@
+import '../styles/Contacts.css'
+
+function Contacts() {
+  return (
+    <div className="contacts" id="contacts">
+      <h2>Контакты</h2>
+      <p><strong>Адрес:</strong> Marina Bay, Singapore</p>
+      <p><strong>Телефон:</strong> +65 1234 5678</p>
+      <p><strong>Email:</strong> info@singaporetours.com</p>
+    </div>
+  )
+}
+
+export default Contacts
