4f444a4fba73def38877cab38f888f22b4ed3688
ayana <ayanayugay25@gmail.com>
2025-05-15 16:11:49 +0600

    Добавлена секция Hero с заголовком, описанием и кнопкой

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Открой Сингапур</h1>
+        <p>Уникальные туры и впечатления в сердце Юго-Восточной Азии</p>
+        <button>Смотреть туры</button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
